734c2ec7c078878910199c57344fdc77321e38a5
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-10-21 14:05:25 +0100

    update permission

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Flask routes"""
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route("/status", methods=['GET'])
 def status():
+    """Return status of api"""
     return jsonify({'status': 'OK'})
