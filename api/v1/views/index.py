b1960ec4223fb917265d476f63bb075d3bc73455
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-10-21 14:17:24 +0100

    add method condition

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Flask routes"""
-from flask import jsonify
+from flask import jsonify, request
 from api.v1.views import app_views
 
 
 @app_views.route("/status", methods=['GET'])
 def status():
     """Return status of api"""
-    return jsonify({'status': 'OK'})
+    if request.method == 'GET':
+        return jsonify({'status': 'OK'})
