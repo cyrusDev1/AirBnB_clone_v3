d76a362533d6396dfd6bd880a0d5b42e4b0b9e63
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-10-21 20:19:06 +0100

    task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask routes states""""
+"""Flask routes states"""
 from flask import jsonify, request, abort, make_response
 from api.v1.views import app_views
 from models import storage
