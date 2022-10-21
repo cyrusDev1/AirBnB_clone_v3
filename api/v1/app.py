3175672e953a3ffca7f728a225feef243cb1da6e
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-10-21 14:21:55 +0100

    update host and port definition

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,14 +3,15 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 app.url_map.strict_slashes = False
 
-HBNB_API_HOST = "0.0.0.0"
-HBNB_API_PORT = 5000
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
 
 
 @app.teardown_appcontext
@@ -23,4 +24,4 @@ def teawrdown(exc):
 
 if __name__ == "__main__":
     """Main flask app"""
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT)
+    app.run(host=host, port=port)
