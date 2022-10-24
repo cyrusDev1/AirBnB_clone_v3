2a069e4e5d945a28d28f97e8ac749ce301679d19
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-10-24 11:45:37 +0100

    task 15: place search #0

--- models/base_model.py
+++ models/base_model.py
@@ -69,8 +69,7 @@ class BaseModel:
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         if HBNB_TYPE_STORAGE == 'db':
-            if "password" in new_dict:
-                del new_dict["password"]
+            del new_dict["password"]
         return new_dict
 
     def delete(self):
